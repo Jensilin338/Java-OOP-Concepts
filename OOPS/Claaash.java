da38711de3c2ebb709596bf7a96ffe6b78fddf68
Jensilin Bino <jensilin338@users.noreply.github.com>
2026-01-25 19:05:31 +0530

    class and objects updated

--- OOPS/Claaash.java
+++ OOPS/Claaash.java
@@ -8,7 +8,11 @@ Classes and Objects:
         (Body)
     } 
 
-    Objects: Objects are real world- entities 
+    Objects: Objects are real world- entities that represent instance of a class.
+    They contain data and behaviour(methods).
+    Syntax:  class-name object-reference = new keyword class-constructor('parameters if any')
+        eg: Person p = new Person();
+    ***Interaction between objects: Objects interact with invoking methods***
 */
 
 class Cars{
@@ -27,6 +31,16 @@ class Cars{
         System.out.println();
     }
 }
+class Person{
+    void speak(){
+        System.out.println("the person is speaking");
+    }
+}
+class Phone{
+    void call(Person p){
+        p.speak();
+    }
+}
 public class Claaash{
     public static void main(String[] args){
         Cars c = new Cars("Swift","20-07-2008","Red");
@@ -34,6 +48,9 @@ public class Claaash{
         Cars c2 = new Cars("Porshe","09-12-2016","Gray");
         Cars c3 = new Cars("Maruthi Suzuki","12-06-2021","White");
         Cars c4 = new Cars("Creta","29-11-2025","Red");
+        Person person = new Person();
+        Phone phone=new Phone();
+        phone.call(person);          //interaction between 2 objects
         c.display();
         c1.display();
         c2.display();
